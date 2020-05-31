be3e35cad031d90849f2806ce2edc7dc1e8d18cf
Servin <osmanovservin1@gmail.com>
2020-05-31 15:56:59 +0300

    Updates

---
+++ precache-manifest.06a8672373bfb00de815edb17ddceac9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4baad2e189ccaf8c9eb0691fe30211f2",
+    "url": "/Countries/index.html"
+  },
+  {
+    "revision": "b310fa9dced013439f99",
+    "url": "/Countries/static/css/4.f908f2ef.chunk.css"
+  },
+  {
+    "revision": "9a275da325c095f67a23",
+    "url": "/Countries/static/css/5.988a49c0.chunk.css"
+  },
+  {
+    "revision": "c07d1842679cc7d71ff1",
+    "url": "/Countries/static/css/6.faaeb93b.chunk.css"
+  },
+  {
+    "revision": "edf02d9fadc8df590b38",
+    "url": "/Countries/static/css/main.6ec7cca5.chunk.css"
+  },
+  {
+    "revision": "837d732f22e4f7a9c627",
+    "url": "/Countries/static/js/2.2b99bf96.chunk.js"
+  },
+  {
+    "revision": "1498606501ebe98ebcaf4e8d6200c692",
+    "url": "/Countries/static/js/2.2b99bf96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3948361560ec179f5855",
+    "url": "/Countries/static/js/3.24fc50c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Countries/static/js/3.24fc50c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b310fa9dced013439f99",
+    "url": "/Countries/static/js/4.86281f6e.chunk.js"
+  },
+  {
+    "revision": "9a275da325c095f67a23",
+    "url": "/Countries/static/js/5.87bdf491.chunk.js"
+  },
+  {
+    "revision": "c07d1842679cc7d71ff1",
+    "url": "/Countries/static/js/6.b6a2b920.chunk.js"
+  },
+  {
+    "revision": "dcfd54bab7bf9e5f8bbb",
+    "url": "/Countries/static/js/7.6fce3e98.chunk.js"
+  },
+  {
+    "revision": "f31f704af7a75e75f9cf",
+    "url": "/Countries/static/js/8.4f8ce888.chunk.js"
+  },
+  {
+    "revision": "edf02d9fadc8df590b38",
+    "url": "/Countries/static/js/main.549e11ed.chunk.js"
+  },
+  {
+    "revision": "78b023f4145c6ba4dcbb",
+    "url": "/Countries/static/js/runtime-main.3a03c88a.js"
+  },
+  {
+    "revision": "cc90bcd2ef2958ffbd663f492f890bd2",
+    "url": "/Countries/static/media/navigation arrow.cc90bcd2.svg"
+  }
+]);
